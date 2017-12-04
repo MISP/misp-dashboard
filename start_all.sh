92ccd8517e6ac5ad780a2a0abacc6d634d32a9c9
Sami Mokaddem <mokaddem.sami@gmail.com>
2017-12-04 14:36:30 +0100

    update: start_all.sh

--- start_all.sh
+++ start_all.sh
@@ -15,10 +15,13 @@ screenName="Misp-Dashboard"
 screen -dmS "$screenName"
 sleep 0.1
 echo -e $GREEN"\t* Launching Redis servers"$DEFAULT
-screen -S "$screenName" -X screen -t "redis-server" bash -c $redis_dir'redis-server '$conf_dir'6250.conf ; read x'
+screen -S "$screenName" -X screen -t "redis-server" bash -c $redis_dir'redis-server '$conf_dir'6250.conf; read x'
 
 echo -e $GREEN"\t* Launching zmq subscriber"$DEFAULT
 screen -S "$screenName" -X screen -t "zmq-subscriber" bash -c './zmq_subscriber.py; read x'
+
+echo -e $GREEN"\t* Launching zmq dispatcher"$DEFAULT
+screen -S "$screenName" -X screen -t "zmq-dispatcher" bash -c './zmq_dispatcher.py; read x'
 
 echo -e $GREEN"\t* Launching flask server"$DEFAULT
 screen -S "$screenName" -X screen -t "flask" bash -c './server.py; read x'
