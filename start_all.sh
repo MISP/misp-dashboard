0fa71408ae76d952761448c9607236988865b64c
Steve Clement <steve@localhost.lu>
2018-01-06 20:51:23 +0100

    - Added debug instructions - Fixed redis-server foo (again) - Added tuneables to start_all.sh - The screen session now tell you what has been started

--- start_all.sh
+++ start_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -e
+set -x
 
 GREEN="\\033[1;32m"
 DEFAULT="\\033[0;39m"
@@ -10,10 +11,15 @@ RED="\\033[1;31m"
 
 redis_dir="${DASH_HOME}/../redis/src/"
 if [ ! -e "${redis_dir}" ]; then
-    [ -f "`which redis-server`" ] && echo "Either ${DASH_HOME}/../redis/src/ does not exist or 'redis-server' is not installed/not on PATH. Please fix and run again." && exit 1
+    [ ! -f "`which redis-server`" ] && echo "Either ${DASH_HOME}/../redis/src/ does not exist or 'redis-server' is not installed/not on PATH. Please fix and run again." && exit 1
     redis_dir=""
 fi
 
+# Configure accordingly, remember: 0.0.0.0 exposes to every active IP interface, play safe and bind it to something you trust and know
+export FLASK_APP=server.py
+export FLASK_DEBUG=0
+export FLASK_PORT=8001
+export FLASK_HOST=127.0.0.1
 
 conf_dir="${DASH_HOME}/config/"
 
@@ -22,13 +28,13 @@ screenName="Misp-Dashboard"
 screen -dmS "$screenName"
 sleep 0.1
 echo -e $GREEN"\t* Launching Redis servers"$DEFAULT
-screen -S "$screenName" -X screen -t "redis-server" bash -c $redis_dir'redis-server '$conf_dir'6250.conf; read x'
+screen -S "$screenName" -X screen -t "redis-server" bash -c $redis_dir'redis-server '$conf_dir'6250.conf && echo "Started Redis" ; read x'
 
 echo -e $GREEN"\t* Launching zmq subscriber"$DEFAULT
-screen -S "$screenName" -X screen -t "zmq-subscriber" bash -c './zmq_subscriber.py; read x'
+screen -S "$screenName" -X screen -t "zmq-subscriber" bash -c 'echo "Starting zmq-subscriber" ; ./zmq_subscriber.py; read x'
 
 echo -e $GREEN"\t* Launching zmq dispatcher"$DEFAULT
-screen -S "$screenName" -X screen -t "zmq-dispatcher" bash -c './zmq_dispatcher.py; read x'
+screen -S "$screenName" -X screen -t "zmq-dispatcher" bash -c 'echo "Starting zmq-dispatcher"; ./zmq_dispatcher.py; read x'
 
 echo -e $GREEN"\t* Launching flask server"$DEFAULT
-screen -S "$screenName" -X screen -t "flask" bash -c './server.py; read x'
+screen -S "$screenName" -X screen -t "flask" bash -c 'echo "Starting Flask Server"; flask run --host=${FLASK_HOST} --port=${FLASK_PORT}; read x'
