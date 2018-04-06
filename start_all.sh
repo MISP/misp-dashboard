90543b716a4a451cbab1d3a213e468b29c2eaa06
Steve Clement <steve@localhost.lu>
2018-04-06 07:27:38 +0200

    - Added some checks if "stuff" is running -- :warning: This works but is not 100% reliable :warning:

--- start_all.sh
+++ start_all.sh
@@ -23,6 +23,9 @@ fi
 [ ! -f "`which redis-server`" ] && echo "'redis-server' is not installed/not on PATH. Please fix and run again." && exit 1
 
 netstat -an |grep LISTEN |grep 6250 |grep -v tcp6 ; check_redis_port=$?
+netstat -an |grep LISTEN |grep 8001 |grep -v tcp6 ; check_dashboard_port=$?
+ps auxw |grep zmq_subscriber.py |grep -v grep ; check_zmq_subscriber=$?
+ps auxw |grep zmq_dispatcher.py |grep -v grep ; check_zmq_dispatcher=$?
 
 # Configure accordingly, remember: 0.0.0.0 exposes to every active IP interface, play safe and bind it to something you trust and know
 export FLASK_APP=server.py
@@ -40,11 +43,26 @@ else
     echo -e $RED"\t* NOT starting Redis server, made a very unrealiable check on port 6250, and something seems to be there… please double check if this is good!"$DEFAULT
 fi
 
-echo -e $GREEN"\t* Launching zmq subscriber"$DEFAULT
-${ENV_PY} ./zmq_subscriber.py &
+sleep 0.1
+if [ "${check_zmq_subscriber}" == "1" ]; then
+    echo -e $GREEN"\t* Launching zmq subscriber"$DEFAULT
+    ${ENV_PY} ./zmq_subscriber.py &
+else
+    echo -e $RED"\t* NOT starting zmq subscriber, made a rather unrealiable ps -auxw | grep for zmq_subscriber.py, and something seems to be there… please double check if this is good!"$DEFAULT
+fi
 
-echo -e $GREEN"\t* Launching zmq dispatcher"$DEFAULT
-${ENV_PY} ./zmq_dispatcher.py &
+sleep 0.1
+if [ "${check_zmq_dispatcher}" == "1" ]; then
+    echo -e $GREEN"\t* Launching zmq dispatcher"$DEFAULT
+    ${ENV_PY} ./zmq_dispatcher.py &
+else
+    echo -e $RED"\t* NOT starting zmq dispatcher, made a rather unrealiable ps -auxw | grep for zmq_dispatcher.py, and something seems to be there… please double check if this is good!"$DEFAULT
+fi
 
-echo -e $GREEN"\t* Launching flask server"$DEFAULT
-${ENV_PY} ./server.py &
+sleep 0.1
+if [ "${check_dashboard_port}" == "1" ]; then
+    echo -e $GREEN"\t* Launching flask server"$DEFAULT
+    ${ENV_PY} ./server.py &
+else
+    echo -e $RED"\t* NOT starting flask server, made a very unrealiable check on port 8001, and something seems to be there… please double check if this is good!"$DEFAULT
+fi
